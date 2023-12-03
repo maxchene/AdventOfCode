290b9c9d54fd80bb656e40c7ea49c8f645b3d6b8
Max <maxime@kaliel.fr>
2023-12-03 09:48:34 +0100

    refacto

--- src/2023/day01.js
+++ src/2023/day01.js
@@ -1,21 +1,17 @@
 import {wordsToNumbers} from 'words-to-numbers';
 
 export function p1(lines){
-    let ans = 0;
-    lines.forEach(line => {
+    return lines.reduce((acc, line) =>{
         const chars = [...line];
         const first = chars.find(item => !isNaN(item));
         const last = chars.findLast(item => !isNaN(item));
-        ans += parseInt(`${first}${last}`);
-    });
-    return ans;
+        return acc += parseInt(`${first}${last}`);
+    },0);
 }
-
-
 
 export function p2(lines){
     const nums = "zero one two three four five six seven eight nine".split(' ');
-    const regex = new RegExp(nums.join('|'), 'gi');
+    const regex = new RegExp(nums.join('|'), 'g');
     const newLines = lines.map(line => {
             return line.replace(regex, (match) =>{
                 return wordsToNumbers(match);
